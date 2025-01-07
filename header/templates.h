d51ab1f9ad97da80dc5495a15e3a47211d397b40
vighneshvn92 <vighneshvn17@gmail.com>
2025-01-07 21:43:29 +0530

    Initial commit

---
+++ header/templates.h
@@ -0,0 +1,8 @@
+// All about the templates
+
+// function template
+
+template<typename T>
+T GetSum(const T &a, const T &b){
+    return a+b;
+}
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include "header/templates.h"
+using namespace std;
+
+int main(){
+    std::cout<<" Understanding concept is very very important!!!!"<<std::endl;
+    std::cout<<" Calling function template"<<std::endl;
+    std::cout<< GetSum(20,25)<<std::endl;
+    }
