d3ed9f0e70eb9cf51ac827f32b88ebdf48311eee
vighneshvn92 <vighneshvn17@gmail.com>
2025-01-14 11:29:09 +0530

    Updated template header

--- header/templates.h
+++ header/templates.h
@@ -1,8 +1,34 @@
-// All about the templates
-
-// function template
+/*All about the templates*/ 
+using namespace std;
+// simple function template
 
 template<typename T>
 T GetSum(const T &a, const T &b){
     return a+b;
+}
+
+// simple template class
+
+template <typename T>
+class MyTempClass{
+    private:
+    T * data;
+    public:
+    MyTempClass(T* value): data(value){}
+    T GetData(){
+        std::cout<<" The value is"<< *data;
+    }
+};
+
+
+template<typename T>
+T Min(T a, T b){
+   return  a < b ? a : b;
+}
+
+// Varadic templates example with min function
+template <typename T, typename... Args>
+T Min(T a, Args... args)
+{
+   return(min(a,Min(args...)));
 }
